69d0e61cef21d15b93dc2483ba46a704b49caf92
Michael Farrelly <mikefarrelly@gmail.com>
2023-08-20 20:27:16 -0600

    add next ui layout

---
+++ app/titlebar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Button,
+} from "@nextui-org/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function TitleBar() {
+    const pathname = usePathname();
+    return (
+        <Navbar>
+            <NavbarBrand>
+                <p className="font-bold text-inherit">ACME</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem isActive={pathname === "/"}>
+                    <Link href="/" aria-current="page">
+                        Timer
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive={pathname === "/stopwatch"}>
+                    <Link href="/stopwatch">Stopwatch</Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
+}
