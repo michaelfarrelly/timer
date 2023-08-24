666bff8a031f2c338d31c13bf0761b6d1aa334fb
Michael Farrelly <mikefarrelly@gmail.com>
2023-08-23 20:01:45 -0600

    tailwindcss: mode jit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,24 @@
-import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/react";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+    // include all tailwind css classes: ref https://www.geeksforgeeks.org/why-are-css-classes-missing-in-production-when-using-tailwind-and-next-js/
+    mode: "jit",
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-        },
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+            }
+        }
     },
     darkMode: "class",
-    plugins: [nextui()],
+    plugins: [nextui()]
 };
 export default config;
