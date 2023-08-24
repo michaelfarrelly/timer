7ac2f3f428f5920bd7d59fb33fa81c60fc121882
Michael Farrelly <mikefarrelly@gmail.com>
2023-08-23 20:09:09 -0600

    include src directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}"
     ],
     theme: {
         extend: {
